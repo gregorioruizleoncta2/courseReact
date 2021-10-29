6b9f431f3261dec164510335014f16206e6139cb
Gregorio Ruiz <gregorioruizleon@gmail.com>
2021-10-29 11:19:10 -0400

    part 1.12 anecdotes paso 1

--- src/index.js
+++ src/index.js
@@ -68,15 +68,42 @@ const Positive = (props) => {
   )
 }
 
+const Anecdotes = (props) =>{
+  const aleatorio = (minimo,maximo)=> Math.floor(Math.random() * ((maximo+1)-minimo)+minimo)
+  
+  let max = props.selected
+  if (props.selected  >=  props.anecdotes.length){
+            max = props.anecdotes.length - 1
+  }
+  return (
+    <div>
+      {props.anecdotes[aleatorio(0,max)]}
+    </div>
+  )
+
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const [selected, setSelected] = useState(0)
+  
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+   
   return (
     
     <div>
+      <Anecdotes anecdotes={anecdotes} selected={selected} />
+      <Button handleClick={() => setSelected(selected + 1)} text="Next Anecdote" />
       <Printer text="Give Feedback" />
       <Button handleClick={() => setGood(good + 1)} text="Good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
